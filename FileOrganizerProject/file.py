128259d62a6a0f00216659a558dcb519dc01b6f1
RonnieInvestments <ronnieinvestments81@gmail.com>
2026-02-12 10:53:34 +0300

    Add code to check for destination paths, and create one if missing.

--- FileOrganizerProject/file.py
+++ FileOrganizerProject/file.py
@@ -1,9 +1,22 @@
 import os
+import shutil
 
-# List all the files
+# list all the files
 def read_folder(folder_path):
     files = os.listdir(folder_path)
-    print(files)
-
+    #print(files)
     return files
+
+# input working_dir, file_name, folder_name
+def move_files(working_dir, file_name, folder_name):
+    destination_path = os.path.join(working_dir, folder_name)
+    # check for destination path
+    # if there is no destination create it
+    source = os.path.join(working_dir, file_name)
+    print("Moving file to ", destination_path)
+
+    if not os.path.isdir:
+        # create it first
+        os.makedirs(destination_path)
+    shutil.move(src = source, dst = destination_path)
 
--- FileOrganizerProject/file_organizer.py
+++ FileOrganizerProject/file_organizer.py
@@ -1,11 +1,12 @@
 from user_folder import get_user_folder
-from file import read_folder
+from file import read_folder, move_files
+import os
 
 # Use all caps to create a constant
-IMAGES = ["jpd", "jpeg", "png"]
-MUSIC = ["mp3", "wav"]
-PDF = ["pdf"]
-ZIP = ["rar", "zip", "tar"]
+IMAGES = [".jpd", ".jpeg", ".png"]
+MUSIC = [".mp3", ".wav"]
+PDF = [".pdf"]
+ZIP = [".rar", ".zip", ".tar"]
 
 
 def main():
@@ -17,5 +18,27 @@ def main():
     for item in items:
         # print a single file in the array
         print("Single item is ", item)
+        split_item = os.path.splitext(item)
+        print("Split item is ", split_item)
+        extension = split_item[1]
+
+        folder_name = "Other"
+
+        if extension in IMAGES:
+            print(f"file {item} is an image")
+            folder_name = "Image"
+        elif extension in MUSIC:
+            print(f"file {item} is a music")
+            folder_name = "Music"
+        elif extension in PDF:
+            print(f"file {item} is a pdf")
+            folder_name = "PDF"
+        elif extension in ZIP:
+            print(f"file {item} is a zip")
+            folder_name = "ZIP"
+        else:
+            print(f"file {item} is unknown or others")
+
+        move_files (file_name = item, working_dir = working_folder, folder_name = folder_name)
 
 main()
